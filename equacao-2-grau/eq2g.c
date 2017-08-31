f83d2f4d2bc938b655557f62df277182596a0306
Fernando Vedoin Garcia <fernando.vgarcia@hotmail.com>
2017-08-30 22:28:38 -0300

    Resolução da equação do segundo grau, independente do delta.

--- equacao-2-grau/eq2g.c
+++ equacao-2-grau/eq2g.c
@@ -10,6 +10,7 @@ void main()
   printf("Digite a, b e c\n");
   scanf("%f%f%f", &a, &b, &c);
   delta = (b*b)-(4*a*c);
+  printf("O valor do delta e': %g\n", delta);
 
   if(delta > 0){
     float r2;
@@ -21,18 +22,22 @@ void main()
 
     printf("Valor de y para primeira raiz: %f\n", y1);
     printf("Valor de y para segunda raiz: %f\n", y2);
-
   }else if(delta == 0){
     r1 = (-b + sqrt(delta))/(2*a);
-    printf("A raíz e': %f\n", r1);
+    printf("A unica raíz e': %f\n", r1);
     y1=a*r1*r1+b*r1+c;
     
     printf("Valor de y para raiz: %f\n", y1);
-
   }else{
-    float aux;
-    r1 = (-b/2*a);
-    aux = sqrt((-delta))/2*a;
-    printf("A raiz e': %g +- %gi\n", r1, aux);
+    float r2;
+    r1 = -b/(2*a); 
+    r2 = sqrt(-delta)/(2*a);
+  
+    float y2;
+    y1 = a*((r1*r1) +2*(r1*r2) - (r2*r2)) + (b*r1)+(b*r2) + c;
+    y2 = a*((r1*r1) -2*(r1*r2) - (r2*r2)) + (b*r1)+(b*-r2) + c;
+
+    printf("As raizes imaginarias da equacao sao (%.2f - %.2fi) e (%.2f + %.2fi)\n",r1,r2,r1,r2);
+    printf("Os valores de y sao %.2f e %.2f\n", y1, y2); 
   }
 }
