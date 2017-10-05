8f22337455d2c0f5412c9b98143acacb760e6ec2
Fernando Vedoin Garcia <fernando.vgarcia@hotmail.com>
2017-10-05 15:07:25 -0300

    Código para calcular a média entre os 5 elementos de um vetor.

---
+++ lab1/exercicios-05out17/media-vetor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+float media(float v[], int n)
+{
+  float aux = 0;
+  for (int i = 0; i < 5; ++i)
+  {
+  	aux += v[i];
+  } 
+  return aux/n;
+}
+void main()
+{
+  float v[5];
+  printf("Digite 5 numeros\n");
+  for (int i = 0; i < 5; ++i)
+  {
+  	scanf("%f", &v[i]);
+  }
+  printf("A media deles e' %f\n", media(v, 5));
+}
