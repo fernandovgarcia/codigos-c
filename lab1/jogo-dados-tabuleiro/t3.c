e97bc089ffde656780e23613682b1071171d4110
Fernando Vedoin Garcia <fernando.vgarcia@hotmail.com>
2017-10-09 10:24:56 -0300

    Função para lançar dados.

---
+++ lab1/jogo-dados-tabuleiro/t3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int nAleatorio(){
+	int n;
+	n = ((rand() % 6) + 1);
+	return n;
+}
+void main(){
+	int i, anterior, tabuleiro[10], num;
+	i = 0;
+	while(i<1){
+		printf("\n");
+	}
+}
