bdfe6a42013ca181c030b468abfcfddb59e622fd
Fernando Vedoin Garcia <fernando.vgarcia@hotmail.com>
2018-05-02 11:43:22 -0300

    Funções para contar numero de nós, verificar se as árvores são iguais e inserir de forma ordenada adicionadas.

--- ed/arvore.c
+++ ed/arvore.c
@@ -15,6 +15,9 @@ int esta_vazia(arvore *a);
 arvore *desaloca_arvore(arvore *a);
 int existe_valor(int valor, arvore *a);
 void imprime(arvore *a);
+int conta_nos(arvore *arvore);
+int sao_iguais(arvore *a, arvore *b);
+arvore *insere_ordenado(int valor, arvore *a);
 
 arvore *cria_vazia(){
   return NULL;
@@ -70,23 +73,101 @@ int existe_valor(int valor, arvore *a){
   	return 0;
 }
 
+int conta_nos(arvore *a){
+  int cont = 0;
+  if (!esta_vazia(a)){
+    cont += 1;
+    cont += conta_nos(a->esq);
+    cont += conta_nos(a->dir);  
+  }else{
+    return cont;
+  }
+  return cont;
+}
+
+int sao_iguais(arvore *a, arvore *b){
+  if(esta_vazia(a) && esta_vazia(b)){
+    return 1;
+  }
+  if(esta_vazia(a) && !esta_vazia(b)){
+    return 0;
+  }
+  if(!esta_vazia(a) && esta_vazia(b)){
+    return 0;
+  }
+  int cont = 0;
+  if(a->valor == b->valor){
+    cont += 1;
+  }
+  cont += sao_iguais(a->esq, b->esq); 
+  cont += sao_iguais(a->dir, b->dir);  
+  if(cont == 3){
+    return 1;
+  }
+  return 0;
+}
+
+arvore *insere_ordenado(int valor, arvore *a){
+  while(a != NULL){
+    if(valor < a->valor){
+      if(a->esq == NULL){
+        arvore *novo = malloc(sizeof(arvore));
+        a->esq = novo;
+        novo->valor = valor;
+        novo->esq = NULL;
+        novo->dir = NULL;
+        return novo;
+      }else{
+        a = a->esq;
+      }
+    }else if(valor > a->valor){
+      if(a->dir == NULL){
+        arvore *novo = malloc(sizeof(arvore));
+        a->dir = novo;
+        novo->valor = valor;
+        novo->esq = NULL;
+        novo->dir = NULL;
+        return novo;
+      }else{
+        a = a->dir;
+      }
+    }
+  }
+}
 
 void main(){
-  arvore *raiz=cria_arvore(5, cria_vazia(), cria_vazia());
-  imprime(raiz); printf("\n");
+  arvore *raiz1=cria_arvore(30, cria_vazia(), cria_vazia());
+  // imprime(raiz1); printf("\n");
 
-  raiz->esq = cria_arvore(35, 
-                     cria_arvore(40, cria_vazia(), cria_vazia()),
-                     cria_arvore(42, cria_vazia(), cria_vazia()));
-  raiz->dir = cria_arvore(85, 
-                     cria_arvore(70, cria_vazia(), cria_vazia()),
-                     cria_arvore(92, cria_vazia(), cria_vazia()));
-  imprime(raiz); printf("\n");
+  raiz1->esq = cria_arvore(9, 
+                     cria_arvore(4, cria_vazia(), cria_vazia()),
+                     cria_arvore(15, cria_vazia(), cria_vazia()));
+  raiz1->dir = cria_arvore(40, 
+                     cria_vazia(),
+                     cria_arvore(31, cria_vazia(), cria_vazia()));
+  imprime(raiz1); printf("\n");
+
+  arvore *raiz2=cria_arvore(5, cria_vazia(), cria_vazia());
+  // imprime(raiz2); printf("\n");
+
+  // raiz2->esq = cria_arvore(35, 
+  //                    cria_arvore(40, cria_vazia(), cria_vazia()),
+  //                    cria_arvore(42, cria_vazia(), cria_vazia()));
+  // raiz2->dir = cria_arvore(85, 
+  //                    cria_arvore(70, cria_vazia(), cria_vazia()),
+  //                    cria_arvore(92, cria_vazia(), cria_vazia()));
+  // imprime(raiz2); printf("\n");
 
   // desaloca_arvore(raiz);
   // imprime(raiz); printf("\n");
-  printf("O VALOR TA NA LISTA? %d\n", existe_valor2(92, raiz));
-  raiz = desaloca_arvore(raiz);
-  imprime(raiz); printf("\n"); 
+  printf("O VALOR TA NA LISTA? %d\n", existe_valor2(92, raiz1));
+  // raiz = desaloca_arvore(raiz);
+  // imprime(raiz); printf("\n"); 
+
+  // printf("%d\n", sao_iguais(raiz1, raiz2));
+
+  insere_ordenado(16, raiz1);
+
+  imprime(raiz1); printf("\n");
 
 }
